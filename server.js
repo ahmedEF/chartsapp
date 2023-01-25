fbd6e58eeb820184e3a4fe7d949788ded770919c
ahmedEF <97808211+ahmedEF@users.noreply.github.com>
2023-01-25 09:33:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,45 +1,56 @@
 const express = require('express');
 const app = express();
 const connectToDb = require('./db');
-// Démarer le serveur sur le port 3000
+
 app.listen(3000, async () => {
     console.log('Server started on port 3000');
+    // connect to the database
     const collection = await connectToDb();
+    // setup endpoints for handling HTTP requests
     setupEndpoints(collection);
 });
-//Definir les endpoints pour le serveur
+
 const setupEndpoints = (collection) => {
+    // endpoint for retrieving data for chart of city and gender
     app.get('/chart', async (req, res) => {
         try {
+            // use MongoDB find method to get data from collection
             const data = await collection.find({}, { projection: { _id: 0,City: 1, Gender: 1 } }).toArray();
-           
+            // send data as json to client
             res.json(data);
           } catch(err) {
           console.log(err);
           res.status(500).send('Error retrieving data from the database');
           }
           });
-          app.get('/RevenueCategorie', async (req, res) => {
+          // endpoint for retrieving data for chart of grossincome and productline
+    app.get('/RevenueCategorie', async (req, res) => {
             try {
+                // use MongoDB find method to get data from collection
                 const data = await collection.find({}, { projection: { _id: 0,Grossincome: 1, Productline: 1 } }).toArray();
+                // send data as json to client
                 res.json(data);
           } catch(err) {
           console.log(err);
           res.status(500).send('Error retrieving data from the database');
           }
           });
-          app.get('/AchatType', async (req, res) => {
+          // endpoint for retrieving data for chart of cogs and customertype
+    app.get('/AchatType', async (req, res) => {
             try {
+                // use MongoDB find method to get data from collection
                 const data = await collection.find({}, { projection: { _id: 0,cogs: 1, Customertype: 1 } }).toArray();
-                res.json(data);
+                // send data as json to client
           } catch(err) {
           console.log(err);
           res.status(500).send('Error retrieving data from the database');
           }        
          });   
+    // endpoint for retrieving data for chart of cogs and customertype
          app.get('/RatingparSexe', async (req, res) => {
           try {
               const data = await collection.find({}, { projection: { _id: 0,Rating: 1, Gender: 1 } }).toArray();
+              // send data as json to client
               res.json(data);
         } catch(err) {
         console.log(err);
