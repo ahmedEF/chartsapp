9c307221d69d5d876c7cd530598a03fa36c910bc
ahmedEF <97808211+ahmedEF@users.noreply.github.com>
2023-01-25 09:54:41 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ChartComponent from './chart2';
+import RevenueCategorie from './chart2';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
