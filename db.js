f342cf911a9ef740ff106dd8554baad7548c9678
ahmedEF <97808211+ahmedEF@users.noreply.github.com>
2023-01-25 17:04:17 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const  uri = "mongodb+srv://AHMEDEF:JQXiFDETa7UtrzJ6@cluster0.51prfli.mongodb.net/?retryWrites=true&w=majority";
-
+// fonction permet de connecter a la base de données mongodb atlas avec un lien uri 
 const connectToDb = async () => {
     try {
         const client = await MongoClient.connect(uri, { useNewUrlParser: true });
@@ -9,5 +9,5 @@ const connectToDb = async () => {
         console.log(err);
     }
 };
-
+// exporter la fonction pour utiliser dans les autres fichiers
 module.exports = connectToDb;
