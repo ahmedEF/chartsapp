9a3ee0bc7e6369eb51375308171ea98b89921aa5
ahmedEF <97808211+ahmedEF@users.noreply.github.com>
2023-01-26 12:15:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,57 +1,50 @@
 const express = require('express');
 const app = express();
 const connectToDb = require('./db');
+//middleware cors 
+const cors = require('cors');
 
-app.listen(3000, async () => {
+//Démarrage du serveur sur le port 3000
+app.listen(3000, async () => {  
     console.log('Server started on port 3000');
-    // connect to the database
+    // connexion à la base de données
 
     const collection = await connectToDb();
 
-    // setup endpoints for handling HTTP requests
+   // configuration des points d'extrémité pour gérer les demandes HTTP
     setupEndpoints(collection);
 });
 
+//permet à l'interface utilisateur de consommer les points d'extrémité
+app.use(cors({origin: 'http://localhost:3001'}));
+
 const setupEndpoints = (collection) => {
-
-    // endpoint for retrieving data for chart of city and gender
-    app.get('/chart', async (req, res) => {
-        try {
-            // use MongoDB find method to get data from collection
-
-            const data = await collection.find({}, { projection: { _id: 0,City: 1, Gender: 1 } }).toArray();
-            // send data as json to client
-
-            res.json(data);
-          } catch(err) {
-          console.log(err);
-          res.status(500).send('Error retrieving data from the database');
-          }
-          });
           
-          // endpoint for retrieving data for chart of grossincome and productline
+
+    // point d'extrémité pour récupérer les données pour le graphique  grossincome et productline
     app.get('/RevenueCategorie', async (req, res) => {
             try {
 
-                // use MongoDB find method to get data from collection
-                const data = await collection.find({}, { projection: { _id: 0,'Goss income': 1, 'Product line': 1 } }).toArray();
+                   // utilisation de la méthode MongoDB find pour récupérer les données de la collection
+                const data = await collection.find({}, { projection: { _id: 0,'gross income': 1,'gross margin percentage':1, 'Product line': 1 } }).toArray();
 
-                // send data as json to client
+                // envoyer les données en format json au client
                 res.json(data);
           } catch(err) {
           console.log(err);
           res.status(500).send('Error retrieving data from the database');
           }
           });
-            // endpoint for retrieving data for chart of cogs and customertype
 
+
+    // point d'extrémité pour récupérer les données pour le graphique de cogs par customertype
     app.get('/AchatType', async (req, res) => {
             try {
 
-                // use MongoDB find method to get data from collection
+                 // utilisation de la méthode MongoDB find pour récupérer les données de la collection
                 const data = await collection.find({}, { projection: { _id: 0,cogs: 1, 'Customer type': 1 } }).toArray();
 
-                // send data as json to client
+                 // envoyer les données en format json au client
                 res.json(data);
           } catch(err) {
           console.log(err);
@@ -59,13 +52,15 @@ const setupEndpoints = (collection) => {
           }        
          });   
 
-    // endpoint for retrieving data for chart of cogs and customertype
+
+
+    // point d'extrémité pour récupérer les données pour le graphique  Rating et sexe(Gender)
     app.get('/RatingparSexe', async (req, res) => {
           try {
-
+                // utilisation de la méthode MongoDB find pour récupérer les données de la collection
               const data = await collection.find({}, { projection: { _id: 0,Rating: 1, Gender: 1 } }).toArray();
 
-              // send data as json to client
+                // envoyer les données en format json au client
 
               res.json(data);
         } catch(err) {
